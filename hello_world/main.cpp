ecf297111c3e915f9417fd3f97ad4430011329e6
yous3fghazyv11 <yous3fghazyv11@gmail.com>
2025-06-09 21:04:40 +0300

    fixing weird comments on github

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -1,8 +1,8 @@
 // simple program to print "Hello, World!" in the terminal
-#include <iostream>				// including functionality from the C++ iostream header file
+#include <iostream>	// including functionality from the C++ iostream header file
 
 int main()
 {
 	std::cout << "Hello, World!" << std::endl; // the line that actually prints the output
-	return 0;								   // value returned to the operation system
+	return 0; // value returned to the operation system
 }
