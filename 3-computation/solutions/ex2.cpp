f6e4e857740aa824f055191300963d7f01fad244
yous3fghazyv11 <yous3fghazyv11@gmail.com>
2025-06-18 03:33:50 +0300

    minor adjustments

--- 3-computation/solutions/ex2.cpp
+++ 3-computation/solutions/ex2.cpp
@@ -2,8 +2,7 @@
 
 int main()
 {
-	for (char c; std::cout << "> ";)
-	{
+	for (char c; std::cout << "> ";) {
         std::cin >> c;
         if (std::cin.eof()) break;
 		std::cout << c << " " << static_cast<int>(c) << '\n';
