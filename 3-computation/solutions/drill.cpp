34b85a148ac6bce6a8016387a99bb825088dec4e
yous3fghazyv11 <yous3fghazyv11@gmail.com>
2025-06-19 19:50:45 +0300

    removing input handeling

--- 3-computation/solutions/drill.cpp
+++ 3-computation/solutions/drill.cpp
@@ -3,27 +3,11 @@
 #include <string>
 #include <vector>
 
-std::string lowercase(std::string s);
-void clear_istream();
 bool valid_unit(std::string unit);
-std::string check_input(double num, std::string unit);
+bool valid_input(double num, std::string unit);
 double convert_to_m(double num, std::string unit);
 double min_element(std::vector<double> v);
 double max_element(std::vector<double> v);
-
-std::string lowercase(std::string input_string) {
-    std::string r;
-    for (char input_char : input_string) {
-        int temp = input_char;
-        if (temp < 97) {
-            char output_char = temp + 32;
-            r += output_char;
-        } else {
-            r += temp;
-        }
-    }
-    return r;
-}
 
 double min_element(std::vector<double> v) {
     double min = std::numeric_limits<double>::max();
@@ -43,36 +27,17 @@ double max_element(std::vector<double> v) {
     return max;
 }
 
-void clear_istream() {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-bool valid_unit(std::string unit) {
+bool valid_input(double num, std::string unit)
+{
+    if (num <= 0) { 
+        return false;
+    }
     std::vector<std::string> valid_units = {"cm", "in", "m", "ft"};
     for (std::string u : valid_units) {
         if (unit == u)
             return true;
     }
     return false;
-}
-
-std::string check_input(double num, std::string unit)
-// we could've made this function a simple bool that returns true if input good,
-// or false if input bad however we want to also check for eof aswell
-{
-    if (std::cin.fail()) {
-        if (std::cin.eof()) return "eof";
-        else return "bad";
-    }
-    if (num <= 0) {
-        return "bad";
-    }
-    unit = lowercase(unit);
-    if (!valid_unit(unit)) {
-        return "bad";
-    }
-    return "success";
 }
 
 double convert_to_m(double num, std::string unit) {
@@ -95,19 +60,11 @@ int main() {
     std::vector<std::string> units;
     std::vector<double> converted_values;
     // main loop
-    while (std::cout << "> ") {
-        // getting input
-        std::cin >> ivalue >> iunit;
-        std::string status = check_input(ivalue, iunit);
-        if (status == "bad") {
-            clear_istream();
+    while (std::cin >> ivalue >> iunit) {
+        if (!valid_input(ivalue, iunit)) {
             std::cerr << "Bad input\n";
             continue;
-        } else if (status == "eof") {
-            std::cout << "\nExiting...\n";
-            break;
         }
-        iunit = lowercase(iunit);
         values.push_back(ivalue);
         units.push_back(iunit);
         double ivalue_in_m = convert_to_m(ivalue, iunit);
